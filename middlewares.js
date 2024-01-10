ca63a360b02af700324c583cb6dea46790214cb4
Abdul_Aziz <57495952+Abdul-Aziz026@users.noreply.github.com>
2024-01-10 12:09:35 +0600

    Delete middlewares.js

--- middlewares.js
+++
@@ -1,8 +0,0 @@
-const isLoggedIn = (req, res, next)=>{
-    if (!req.isAuthenticated()) {
-        req.flash("error", "Please Login for create Post");
-        return res.redirect("/home/login");
-    }
-    next();
-}
-module.exports = isLoggedIn;
